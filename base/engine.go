8da232d62871290502b8405b30f86dea2a57eb5a
Amir <soltamir194@gmail.com>
2024-03-21 08:47:08 -0400

    added ping/pong

--- base/engine.go
+++ base/engine.go
@@ -3,6 +3,8 @@ package base
 // Webframework, handles mostly routes and requests
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +12,8 @@ func (base *Base) loadEngine() {
 	gin.SetMode(gin.DebugMode)
 	// gin.DisableConsoleColor()
 	engine := gin.Default()
+
+	engine.GET("/ping", func(ctx *gin.Context) { ctx.String(http.StatusOK, "pong") })
 
 	base.Engine = engine
 }
